80ba3e89b96ebf4c34383c58290090ae1bf93137
seongjin <shawn95981@gmail.com>
2023-04-06 21:17:27 -0400

    commented out connecting to mongodb named login-db in mongo.js

--- backend/mongo.js
+++ backend/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://1270.0.1:27017/login-db')
-.then(() => {console.log("mongodb is connected through mongoose!")})
-.catch(() => {console.log("failed")})
+// mongoose.connect('mongodb://1270.0.1:27017/login-db')
+// .then(() => {console.log("mongodb is connected through mongoose!")})
+// .catch(() => {console.log("failed")})
 
 const loginInfoSchema = new mongoose.Schema({
     loginInfo : {
