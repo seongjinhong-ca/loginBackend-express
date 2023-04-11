e673a2e61ed085fc7cedaf2b8fcd33e7a020eec5
seongjin <shawn95981@gmail.com>
2023-04-11 18:13:27 -0400

    starting page of forntend view and also tried testing fetching data from backend end-points

--- frontend/my-app/src/components/views/LandingPage/LandingPage.js
+++ frontend/my-app/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,23 @@
 import React, {useEffect} from 'react'
-// import axios from 'axios';
+import axios from 'axios';
 import { getInstance } from '../../../utils/api_client';
 
 
 function LandingPage() {
     useEffect(()=>{
-        getInstance().get('/api/hello')
+        // getInstance().get('/api/hello')
+        axios.get('/api/hello')
         .then(response => console.log(response.data))
     },[])
   return (
     <div>
         This is landing page
+        <div style={{
+          display:'flex', justifyContent:"center", alignItems:"center",
+          width:'100%', height: '100vh'
+        }}>
+          <h2>시작 페이지</h2>
+        </div>
     </div>
   )
 }
