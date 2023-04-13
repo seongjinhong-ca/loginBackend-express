4001c23ea08114170375b3a4f1b06445e2f6e732
seongjin <shawn95981@gmail.com>
2023-04-12 21:23:52 -0400

    learned functor === map

---
+++ docs/frontend/haskell/functor.hs
@@ -0,0 +1,5 @@
+-- https://velog.io/@yokitomi/you-already-know-functor
+let b=a+1
+-- https://www.adit.io/posts/2013-04-17-functors,_applicatives,_and_monads_in_pictures.html
+-- http://www.cs.utoronto.ca/~trebla/x/0J50KOV5UH/CSCC24-2020-Winter/06-fam.html
+-- https://wikidocs.net/1469
